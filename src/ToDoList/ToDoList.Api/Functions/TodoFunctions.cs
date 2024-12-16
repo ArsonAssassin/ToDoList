4d15f3d1c8ac3a91a8d20f63902dc15a8e239b7a
Thepharaoh49 <Thepharaoh49@hotmail.co.uk>
2024-12-16 19:03:13 +0000

    add swagger parameters and responses

--- src/ToDoList/ToDoList.Api/Functions/TodoFunctions.cs
+++ src/ToDoList/ToDoList.Api/Functions/TodoFunctions.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using ToDoList.Api.Data;
+using ToDoList.Api.Models;
 
 namespace ToDoList.Api.Functions
 {
@@ -21,6 +24,8 @@ namespace ToDoList.Api.Functions
 
         [Function("GetTodoItem")]
         [OpenApiOperation("GetTodoItem")]
+        [OpenApiParameter("id", In = ParameterLocation.Path, Required = true, Type = typeof(Guid))]
+        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(TodoItem))]
         public async Task<HttpResponseData> GetTodoItem([HttpTrigger(AuthorizationLevel.Function, "get", Route = "todo/{id}")] HttpRequestData req)
         {
             throw new NotImplementedException();
@@ -28,6 +33,7 @@ namespace ToDoList.Api.Functions
 
         [Function("GetAllTodoItems")]
         [OpenApiOperation("GetAllTodoItems")]
+        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IEnumerable<TodoItem>))]
         public async Task<HttpResponseData> GetAllTodoItems([HttpTrigger(AuthorizationLevel.Function, "get", Route = "todo")] HttpRequestData req)
         {
             throw new NotImplementedException();
@@ -35,6 +41,14 @@ namespace ToDoList.Api.Functions
 
         [Function("QueryTodoItems")]
         [OpenApiOperation("QueryTodoItems")]
+        [OpenApiParameter("isCompleted", In = ParameterLocation.Query, Required = false, Type = typeof(bool?))]
+        [OpenApiParameter("createdAfterDate", In = ParameterLocation.Query, Required = false, Type = typeof(DateTime?))]
+        [OpenApiParameter("createdBeforeDate", In = ParameterLocation.Query, Required = false, Type = typeof(DateTime?))]
+        [OpenApiParameter("completedAfterDate", In = ParameterLocation.Query, Required = false, Type = typeof(DateTime?))]
+        [OpenApiParameter("completedBeforeDate", In = ParameterLocation.Query, Required = false, Type = typeof(DateTime?))]
+        [OpenApiParameter("titleContains", In = ParameterLocation.Query, Required = false, Type = typeof(string))]
+        [OpenApiParameter("descriptionContains", In = ParameterLocation.Query, Required = false, Type = typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IEnumerable<TodoItem>))]
         public async Task<HttpResponseData> QueryTodoItems([HttpTrigger(AuthorizationLevel.Function, "get", Route = "todo/query")] HttpRequestData req)
         {
             throw new NotImplementedException();
@@ -42,6 +56,9 @@ namespace ToDoList.Api.Functions
 
         [Function("UpdateTodoItem")]
         [OpenApiOperation("UpdateTodoItem")]
+        [OpenApiParameter("id", In = ParameterLocation.Path, Required = true, Type = typeof(Guid))]
+        [OpenApiRequestBody("application/json", typeof(TodoItem))]
+        [OpenApiResponseWithoutBody(HttpStatusCode.OK)]
         public async Task<HttpResponseData> UpdateTodoItem([HttpTrigger(AuthorizationLevel.Function, "put", Route = "todo/{id}")] HttpRequestData req)
         {
             throw new NotImplementedException();
