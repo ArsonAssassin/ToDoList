dc84c3f4cf39f8636b9a1aa3396a0c918ef8e379
Thepharaoh49 <Thepharaoh49@hotmail.co.uk>
2024-12-16 18:52:24 +0000

    Swagger setup and function stubs

--- src/ToDoList/ToDoList.Api/Functions/TodoFunctions.cs
+++ src/ToDoList/ToDoList.Api/Functions/TodoFunctions.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +18,34 @@ namespace ToDoList.Api.Functions
         {
             _todoRepository = todoRepository;
         }
+
+        [Function("GetTodoItem")]
+        [OpenApiOperation("GetTodoItem")]
+        public async Task<HttpResponseData> GetTodoItem([HttpTrigger(AuthorizationLevel.Function, "get", Route = "todo/{id}")] HttpRequestData req)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Function("GetAllTodoItems")]
+        [OpenApiOperation("GetAllTodoItems")]
+        public async Task<HttpResponseData> GetAllTodoItems([HttpTrigger(AuthorizationLevel.Function, "get", Route = "todo")] HttpRequestData req)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Function("QueryTodoItems")]
+        [OpenApiOperation("QueryTodoItems")]
+        public async Task<HttpResponseData> QueryTodoItems([HttpTrigger(AuthorizationLevel.Function, "get", Route = "todo/query")] HttpRequestData req)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Function("UpdateTodoItem")]
+        [OpenApiOperation("UpdateTodoItem")]
+        public async Task<HttpResponseData> UpdateTodoItem([HttpTrigger(AuthorizationLevel.Function, "put", Route = "todo/{id}")] HttpRequestData req)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
---
+++ src/ToDoList/ToDoList.Api/Services/OpenApi/OpenApiSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ToDoList.Api.Services.OpenApi
+{
+    public class OpenApiSettings : DefaultOpenApiConfigurationOptions
+    {
+        public override OpenApiInfo Info { get; set; } = new OpenApiInfo
+        {
+            Title = "Todo List API",
+            Version = "1.0.0",
+            Description = @"
+                This API serves data for managing Todo Lists as a backend for TodoListUI.
+            "           
+        };
+
+    }
+}
